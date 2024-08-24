c18c7f5bc7acd4fd20140b498543d61439c2b780
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-08-24 14:35:12 +0300

    Fix like model Schema

--- src/models/like.models.js
+++ src/models/like.models.js
@@ -1,31 +1,33 @@
 import mongoose, { Schema } from "mongoose";
 
 const likesSchema = new Schema({
-  video: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Video",
-    }
-  ],
-  comment: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Comment",
-    }
-  ],
-  tweet: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Tweet",
-    }
-  ],
-  likedBy: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    }
-  ],
+  video:
+  {
+    type: Schema.Types.ObjectId,
+    ref: "Video",
+  }
+  ,
+  comment:
+  {
+    type: Schema.Types.ObjectId,
+    ref: "Comment",
+  }
+  ,
+  tweet:
+  {
+    type: Schema.Types.ObjectId,
+    ref: "Tweet",
+  }
+  ,
+  likedBy:
+  {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  }
+  ,
 }, { timestamps: true })
+
+export const Like = mongoose.model("Like", likesSchema)
 
 /**
  * id string pk
